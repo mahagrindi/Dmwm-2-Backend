ffc6dedf2aa53e3bae5829b0a2f1ae6d8db35686
ach02raf <ach02raf@gmail.com>
2023-03-27 23:01:34 +0100

    image upload

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const PostRoutes = require("./routes/Posts");
 const cors = require("cors");
 
 require("dotenv").config();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
