8a188b38ab1dc22ad5cfd22695d608e5e9fb6ad1
ach02raf <ach02raf@gmail.com>
2023-04-05 16:50:38 +0100

    4-5-2023 v2

--- Module-Django-App/ImageVerification/views.py
+++ Module-Django-App/ImageVerification/views.py
@@ -2,7 +2,7 @@ import requests
 from django.shortcuts import render
 from django.http import HttpResponse
 import base64
-import cv2
+from .models import compareImage
 
 
 def home(request):
@@ -19,13 +19,9 @@ def home(request):
             img_data_uri = base64.b64encode(img_bytes).decode('utf-8')
             imagesTab.append(img_data_uri)
         for image in imagesTab:
-            if image == one_img_data_uri:
+            if compareImage(image, one_img_data_uri) == "true":
                 exist = 'true'
-
-        context = {
-            'response': exist
-        }
-        return render(request, 'home.html', context)
+        return HttpResponse(exist)
     if request.method == 'GET':
         return HttpResponse("Not Get request")
 
