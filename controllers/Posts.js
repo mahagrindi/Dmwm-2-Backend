2f1cd348d7bcd84d40125d9e3f29517920f5bf97
mahagrindi <grindi.maha@gmail.com>
2023-04-06 09:23:46 +0100

    post

--- controllers/Posts.js
+++ controllers/Posts.js
@@ -5,19 +5,112 @@ const path = require("path");
 const fs = require("fs");
 var imgModel = require("../models/Image");
 var publicationModel = require("../models/publication");
-const axios = require('axios');
+
 // Step 7 - the GET request handler that provides the HTML UI
 
-exports.getImage = async (req, res) => {
-  
-
+/* exports.getImage = async (req, res) => {
   try {
-    const images = await imgModel.findById(req.body.id);
-    res.send(images);
-    
+    console.log(req.query.id);
+    const image = await imgModel.findById(req.query.id);
+    if (!image) {
+      return res.status(404).json({error: "Image not found"});
+    }
+    res.send(image.name);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+};
+ */
+
+exports.addreaction = async (req, res) => {
+  try {
+    const publication = await publicationModel.findById(req.body.publicationId);
+    if (!publication) {
+      throw new Error("Publication not found");
+    }
+
+    const idUser = req.body.UserId;
+    const reactionIndex = publication.reaction.findIndex(
+      (r) => r.idUser === idUser
+    );
+    if (reactionIndex > -1) {
+      publication.reaction.splice(reactionIndex, 1);
+    } else {
+      publication.reaction.push({idUser});
+    }
+
+    await publication.save();
+    res.send("ok");
   } catch (error) {
-    console.log(error);
-    res.status(500).send("Server Error");
+    throw new Error(error.message);
+  }
+};
+
+exports.addcomment = async (req, res) => {
+  try {
+    const publication = await publicationModel.findById(req.body.idPub);
+    if (!publication) {
+      throw new Error("Publication not found");
+    }
+
+    publication.commentaires.push({
+      idUser: req.body.iduser,
+      comment: req.body.comm,
+      reaction: 0,
+      comments: [],
+    });
+
+    await publication.save();
+    res.send("ok");
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+
+exports.commreaction = async (req, res) => {
+  try {
+    const publication = await publicationModel.findByIdAndUpdate(
+      req.body.idPub,
+      { $inc: { [`commentaires.${req.body.commindex}.reaction`]: 1 } } 
+    );
+
+    if (!publication) {
+      return res.status(404).send("Publication not found");
+    }
+
+    res.status(200).send("Comment reaction updated");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
+  }
+};
+
+exports.addcommentReply = async (req, res) => {
+  try {
+    const publication = await publicationModel.findByIdAndUpdate(
+      req.body.idPub,
+      {
+        $push: {
+          [`commentaires.${req.body.commindex}.comments`]: {
+            userId: req.body.iduser,
+            text: req.body.reply,
+            reaction: 0,
+          },
+        },
+      }
+    );
+    
+
+    if (!publication) {
+      return res.status(404).send("Publication not found");
+    }
+
+    res.status(200).send("Comment reaction updated");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server error");
   }
 };
 
@@ -32,93 +125,76 @@ exports.getPublication = async (req, res) => {
   }
 };
 
-
 exports.PostPublication = async (req, res) => {
-  const options = { month: "2-digit", day: "2-digit", year: "numeric" };
+  const options = {month: "2-digit", day: "2-digit", year: "numeric"};
   //  const currentDate = new Date().toLocaleString(options);
   let currentDate = Date.now();
-  
+
   var hashtagList = [];
-  if(req.body.hashtags){
-   // Convert hashtags to an array if it's not already an array
-   const hashtags = Array.isArray(req.body.hashtags) ? req.body.hashtags : [req.body.hashtags];
-  
-   for (const tag of hashtags) {
-    const existingTag = await Hashtag.findOne({ tag_name: tag });
-    let tagId;
-  
-    if (!existingTag) {
-      // If tag doesn't exist, create a new tag and save it to the database
-      const newTag = new Hashtag();
-      newTag.tag_name = tag;
-      newTag.copyrightChecked = req.body.copyrightChecked;
-      await newTag.save();
-      tagId = newTag._id;
-    } else {
-      // If tag exists, get its ID
-      tagId = existingTag._id;
+  if (req.body.hashtags) {
+    // Convert hashtags to an array if it's not already an array
+    const hashtags = Array.isArray(req.body.hashtags)
+      ? req.body.hashtags
+      : [req.body.hashtags];
+
+    for (const tag of hashtags) {
+      const existingTag = await Hashtag.findOne({tag_name: tag});
+      let tagId;
+
+      if (!existingTag) {
+        // If tag doesn't exist, create a new tag and save it to the database
+        const newTag = new Hashtag();
+        newTag.tag_name = tag;
+        newTag.copyrightChecked = req.body.copyrightChecked;
+        await newTag.save();
+        tagId = newTag._id;
+      } else {
+        // If tag exists, get its ID
+        tagId = existingTag._id;
+      }
+
+      // Add the ID to the list of hashtags for the post
+      hashtagList.push(tagId);
     }
-  
-    // Add the ID to the list of hashtags for the post
-    hashtagList.push(tagId);
   }
-}
-   
 
   var ImgList = [];
- if( req.files.images){
-  for (var element of req.files.images) {
-    let img = new imgModel({
-      name: element.filename,
-      img: {
-        data: fs.readFileSync(
-          path.join(
-            path.dirname(require.main.filename),
-            "uploads",
-            element.filename
-          )
-        ),
-        contentType: "image/png",
-      },
-    });
+  if (req.files.images) {
+    for (var element of req.files.images) {
+      let img = new imgModel({
+        name: element.filename,
+        img: {
+          data: fs.readFileSync(
+            path.join(
+              path.dirname(require.main.filename),
+              "uploads",
+              element.filename
+            )
+          ),
+          contentType: "image/png",
+        },
+      });
 
-    
-axios.post('http://127.0.0.1:8000', {
-  image: img
-})
-  .then( async (response) => {
-    console.log(response.data);
-    await img.save().then((res) => {
-      console.log(res._id);
-      ImgList.push(res._id);
-      console.log(ImgList);
-    });
-  })
-  .catch((error) => {
-    console.error(error);
-  });
-
- 
-   
-    
-   
-  }}
+      await img.save().then((res) => {
+        console.log(res._id);
+        ImgList.push({idimg: res._id, imgName: element.filename});
+        console.log(ImgList);
+      });
+    }
+  }
   var post = new publicationModel({
     Id_user: req.body.Id_user,
     text: req.body.text,
     date: currentDate,
 
     img: ImgList,
-    hashtag: hashtagList ,
+    hashtag: hashtagList,
   });
 
-  
   post.save().then((result) => {
-  res.status(200).json({ message: "post added" });
-});
-
+    res.status(200).json({message: "post added"});
+  });
 };
-
 
 exports.getAllImages = async (req, res) => {
   try {
@@ -129,7 +205,6 @@ exports.getAllImages = async (req, res) => {
   }
 };
 
- 
 // *************************
 // ********* tags ***********
 // **************************
@@ -146,9 +221,9 @@ exports.GetTag = async (req, res) => {
 };
 exports.AddTags = async (req, res) => {
   // Extract the tag name from the request b
-  const { tagname } = req.body;
+  const {tagname} = req.body;
   console.log(req.body);
-  const { tagn } = req.params;
+  const {tagn} = req.params;
   // Add this line to log the request body
   try {
     if (!tagn) {
@@ -158,7 +233,7 @@ exports.AddTags = async (req, res) => {
     }
 
     // Check if tag already exists
-    const existingTag = await Hashtag.findOne({ tag_name: tagn });
+    const existingTag = await Hashtag.findOne({tag_name: tagn});
     if (existingTag) {
       // If tag already exists, return a success response
       return res.status(200).json({
--- routes/Posts.js
+++ routes/Posts.js
@@ -2,16 +2,17 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const PostControllers = require("../controllers/Posts");
- 
 
 const upload = require("../Midellware/multer");
- 
+
 router.get("/getImages", PostControllers.getAllImages);
-router.get("/getOneImage", PostControllers.getImage);
 
-
+//router.get("/getImage", PostControllers.getImage);
 router.get("/getPublications", PostControllers.getPublication);
-
+router.put("/reaction", PostControllers.addreaction);
+router.put("/addcomment", PostControllers.addcomment);
+router.put("/commreaction", PostControllers.commreaction);
+router.put("/addcommentReply", PostControllers.addcommentReply);
 router.post(
   "/PostPublication",
   upload.fields([{name: "images"}]),
