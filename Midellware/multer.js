cde2b423ecd5f6b20293aeb5935b46ad8b34033c
mahagrindi <grindi.maha@gmail.com>
2023-04-18 11:54:32 +0100

    post

--- Midellware/multer.js
+++ Midellware/multer.js
@@ -3,6 +3,7 @@ const multer = require("multer");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "uploads");
+    
   },
   filename: function (req, file, cb) {
     if (file.fieldname == "images") {
